db1824509606d08026cf40719c83b19098e11052
Animesh Joshi <animeshjoshi2007@gmail.com>
2022-05-05 18:12:50 +0530

    Added ability to download playlists and updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-#YouTube-Downloader
+# YouTube-Downloader
 
-Youtube video downloader made with tkinter, pytube
+Youtube video downloader made with tkinter and pytube.
+It also supports playlists.
+The program uses multiprocesses so that the program doesn't
+stop working or crash while downloading. Multiple videos can be downloaded
+at one time because of this. Downloading videos does take a lot of time
+so it might take some time for them to download.
+
+## Requirements
+
+The program uses Python 3.10 with the only external package being pytube.
+To install run `pip3 install pytube` in the terminal.
+
+### Note
+
+If you are getting some error like this
+
+`pytube.exceptions.RegexMatchError: get_throttling_function_name: could not find match for multiple
+`
+
+then refer to [this Github issue](https://github.com/pytube/pytube/issues/1293).
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from tkinter import *
 from tkinter import messagebox, ttk
 import multiprocessing
+import os
 
 import pytube.exceptions
-from pytube import YouTube
+from pytube import YouTube, Playlist
+
+
+def YouTube_process(url, audio_only=False, file_extension="mp4"):
+    p = multiprocessing.Process(target=YouTube_downloader, args=(url, audio_only, file_extension,))
+
+    p.start()
 
 
 def YouTube_downloader(url, audio_only=False, file_extension="mp4"):
@@ -33,23 +40,81 @@ def YouTube_downloader(url, audio_only=False, file_extension="mp4"):
         messagebox.showerror('Error', str(e))
 
 
-def YouTube_process(url, audio_only=False, file_extension="mp4"):
-    p = multiprocessing.Process(target=YouTube_downloader, args=(url, audio_only, file_extension,))
+def Playlist_downloader(url, audio_only=False, file_extension="mp4"):
+    try:
+
+        p = Playlist(url)
+
+        try:
+            os.mkdir(f"{p.title}")
+        except FileExistsError:
+            pass
+
+        for video in p.videos:
+
+            if audio_only:
+                video.streams.get_audio_only().download(output_path=f'{p.title}/')
+
+            else:
+                try:
+
+                    video.streams.filter(file_extension=file_extension, only_audio=False).first().download(
+                        output_path=f'{p.title}/')
+
+                except AttributeError:
+                    messagebox.showerror('Error', 'Enter valid file extension')
+
+        messagebox.showinfo('Video Downloader', 'Done!')
+
+    except pytube.exceptions.RegexMatchError:
+        messagebox.showerror('Error', 'Enter valid url')
+
+    except pytube.exceptions.PytubeError as e:
+        messagebox.showerror('Error', str(e))
+
+
+def Playlist_process(url, audio_only=False, file_extension="mp4"):
+    p = multiprocessing.Process(target=Playlist_downloader, args=(url, audio_only, file_extension,))
 
     p.start()
 
 
+def distinguisher(url):
+    if "&list=" or "playlist" in url:
+        return "playlist"
+    else:
+        return "video"
+
+
 def show_info(url):
     try:
-        yt = YouTube(url)
+        if distinguisher(url) == "video":
+            yt = YouTube(url)
 
-        secs = yt.length % 60
-        minutes = (yt.length - secs) / 60
+            secs = yt.length % 60
+            minutes = (yt.length - secs) / 60
 
-        messagebox.showinfo("Video", f"Title: {yt.title}\nBy: {yt.author}\nViews: {yt.views}\nLength {minutes}:{secs}")
+            messagebox.showinfo("Video",
+                                f"Title: {yt.title}\nBy: {yt.author}\nViews: {yt.views}\nLength {minutes}:{secs}")
+        else:
+            p = Playlist(url)
+
+            messagebox.showinfo("Playlist",
+                                f"Title: {p.title}\nOwner: {p.owner}\nViews: {p.views}\nLength: {p.length}")
 
     except pytube.exceptions.RegexMatchError:
         messagebox.showerror('Error', 'Enter valid url')
+
+
+def download(url, audio_only=False, file_extension="mp4"):
+
+    if distinguisher(url) == "playlist":
+
+        Playlist_process(url, audio_only, file_extension)
+
+    else:
+
+        YouTube_process(url, audio_only, file_extension)
 
 
 def main():
@@ -79,7 +144,7 @@ def main():
     URL.place(width=500, height=30, x=0, y=30)
 
     Button(window, text='Download!',
-           command=lambda: YouTube_process(URL.get(), audio.get(), combo.get())).place(
+           command=lambda: download(URL.get(), audio.get(), combo.get())).place(
         width=100,
         height=30, x=150,
         y=60)
@@ -97,4 +162,3 @@ def tkinter_process():
 
 if __name__ == '__main__':
     tkinter_process()
-
